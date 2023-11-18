d056dedf1428d5e99f8f200648b19af8169ae728
Lucas de Almeida Martins <lucasadvh@gmail.com>
2023-11-18 13:11:55 -0300

    Third commit

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 // Script que contém uma função que ajuda a validar uma idade de usuário
-function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+function age(year_birthday, month_birthday, day_birthday) {
     var d = new Date,
-        ano_atual = d.getFullYear(),
-        mes_atual = d.getMonth() + 1,
-        dia_atual = d.getDate(),
+        current_year = d.getFullYear(),
+        current_month = d.getMonth() + 1,
+        current_day = d.getDate(),
 
-        ano_aniversario = +ano_aniversario,
-        mes_aniversario = +mes_aniversario,
-        dia_aniversario = +dia_aniversario,
+        year_birthday = +year_birthday,
+        month_birthday = +month_birthday,
+        day_birthday = +day_birthday,
 
-        quantos_anos = ano_atual - ano_aniversario;
+        how_many_years = current_year - year_birthday;
 
-    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
-        quantos_anos--;
+    if (current_month < month_birthday || current_month == month_birthday && current_day < day_birthday) {
+        how_many_years--;
     }
 
-    return quantos_anos < 0 ? 0 : quantos_anos;
+    return how_many_years < 0 ? 0 : how_many_years;
 }
 
 
 
-console.log(idade(1980, 12, 11)); //  33
+console.log(age(1980, 12, 11)); //  33
 
-console.log(idade(2011, 2, 15));  // 2
+console.log(age(2011, 2, 15));  // 2
 
-console.log(idade(1993, 31, 5));  // 20
-
-
-/*
-
-function idade(ano_nascimento, mes_nascimento, dia_nascimento){
-
-}
+console.log(age(1993, 31, 5));  // 20
 
 
-*/
